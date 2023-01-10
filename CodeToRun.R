bf71d53ac04b0d968785cd555c8d11841bb83856
daniellenewby <danielle.newby@ndorms.ox.ac.uk>
2023-01-10 14:06:59 +0000

    update

--- CodeToRun.R
+++ CodeToRun.R
@@ -19,7 +19,7 @@ library(ggplot2)
 
 # database metadata and connection details -----
 # The name/ acronym for the database (for CPRD AURUM please use "CPRDAurum" and for SIDIAP use "SIDIAP" )
-db.name<-"CPRDAurum"
+db.name<-"..."
 
 # Set output folder location -----
 # the path to a folder where the results from this analysis will be saved
@@ -30,18 +30,18 @@ output.folder<-here("Results", db.name)
 # Specify databaseConnector connection details -----
 # database connection details
 # connect to database
-user<-Sys.getenv("DB_USER")
-password<- Sys.getenv("DB_PASSWORD")
-port<-Sys.getenv("DB_PORT") 
-host<-Sys.getenv("DB_HOST") 
-server_dbi<-Sys.getenv("DB_SERVER_cdm_aurum_202106_dbi")
+user<-"..."
+password<- "..."
+port<-"..."
+host<-"..."
+server_dbi<-"..."
 
 
 # Specify cdm_reference via DBI connection details -----
 # In this study we also use the DBI package to connect to the database
 # set up the dbConnect details below (see https://dbi.r-dbi.org/articles/dbi for more details)
 # you may need to install another package for this (although RPostgres is included with renv in case you are using postgres)
-db <- dbConnect(RPostgres::Postgres(),
+db <- dbConnect("...",
                 dbname = server_dbi,
                 port = port,
                 host = host, 
@@ -50,19 +50,19 @@ db <- dbConnect(RPostgres::Postgres(),
 
 # Set database details -----
 # The name of the schema that contains the OMOP CDM with patient-level data
-cdm_database_schema<-"public"
+cdm_database_schema<-"..."
 
 # The name of the schema that contains the vocabularies 
 # (often this will be the same as cdm_database_schema)
 vocabulary_database_schema<-cdm_database_schema
 
 # The name of the schema where results tables will be created 
-results_database_schema<-"results"
+results_database_schema<-"..."
 
 # Name of outcome table in the result table where the outcome cohorts will be stored
 # Note, if there is an existing table in your results schema with the same names
 # it will be overwritten 
-outcome_table_stem<-"dementadruginc"
+outcome_table_stem<-"..."
 
 # create cdm reference ----
 cdm <- CDMConnector::cdm_from_con(con = db, 
@@ -75,9 +75,6 @@ cdm$person %>%
   tally()
 
 # Run the study ------
-# create_outcome_cohorts<-FALSE # set to false if already instantiated
-# create_strata_cohorts<-FALSE # set to false if already instantiated
-
 source(here("RunStudy.R"))
 # after the study is run you should have a zip folder in your output folder to share
 
