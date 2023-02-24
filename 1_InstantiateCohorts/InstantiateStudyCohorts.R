7e8acb7bd4aa5a0bb70e2d60e3d2654ba957ff32
daniellenewby <danielle.newby@ndorms.ox.ac.uk>
2023-02-24 16:06:36 +0000

    update for debugging

--- 1_InstantiateCohorts/InstantiateStudyCohorts.R
+++ 1_InstantiateCohorts/InstantiateStudyCohorts.R
@@ -34,36 +34,36 @@ cdm <- generateCohortSet(cdm, strata_cohorts,
 info(logger, "- got strata")
 
 # instantiate feature cohorts (disease)
-info(logger, "- getting feature for diseases definitions")
-
-disease_cohorts <- readCohortSet(here(
-  "1_InstantiateCohorts",
-  "DiseaseCohorts"
-))
-
-info(logger, "- getting features: diseases")
-
-cdm <- generateCohortSet(cdm, disease_cohorts,
-                         cohortTableName = feature_disease_table_name,
-                         overwrite = TRUE
-)
-
-info(logger, "- got features for diseases")
-
-# instantiate feature cohorts (medications)
-info(logger, "- getting feature for medications definitions")
-
-medication_cohorts <- readCohortSet(here(
-  "1_InstantiateCohorts",
-  "MedicationCohorts"
-))
-
-info(logger, "- getting features: medications")
-
-cdm <- generateCohortSet(cdm, medication_cohorts,
-                         cohortTableName = feature_medication_table_name,
-                         overwrite = TRUE
-)
-
-info(logger, "- got features for medications")
+# info(logger, "- getting feature for diseases definitions")
+# 
+# disease_cohorts <- readCohortSet(here(
+#   "1_InstantiateCohorts",
+#   "DiseaseCohorts"
+# ))
+# 
+# info(logger, "- getting features: diseases")
+# 
+# cdm <- generateCohortSet(cdm, disease_cohorts,
+#                          cohortTableName = feature_disease_table_name,
+#                          overwrite = TRUE
+# )
+# 
+# info(logger, "- got features for diseases")
+# 
+# # instantiate feature cohorts (medications)
+# info(logger, "- getting feature for medications definitions")
+# 
+# medication_cohorts <- readCohortSet(here(
+#   "1_InstantiateCohorts",
+#   "MedicationCohorts"
+# ))
+# 
+# info(logger, "- getting features: medications")
+# 
+# cdm <- generateCohortSet(cdm, medication_cohorts,
+#                          cohortTableName = feature_medication_table_name,
+#                          overwrite = TRUE
+# )
+# 
+# info(logger, "- got features for medications")
 
